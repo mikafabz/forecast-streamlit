c47419e45e6b7247b4b7f6063d29258da0352c8d
Fabrizio Michels <mikafabz@gmail.com>
2025-06-19 21:37:54 -0300

    Add files via upload

---
+++ forecast_app.py
@@ -0,0 +1,7 @@
+
+import streamlit as st
+
+st.set_page_config(page_title='Forecast BTC e Ações', layout='centered')
+
+st.title("📈 Previsão de Preços - BTC e Ações")
+st.markdown("Este app prevê os próximos 14 dias de preços para Bitcoin e ações usando aprendizado de máquina.")
